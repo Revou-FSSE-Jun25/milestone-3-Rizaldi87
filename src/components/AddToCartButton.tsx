1abd168577f9afedd637ebd4d51415d3bd02b3e8
Rizaldi87 <rizaldiibnum@gmail.com>
2025-10-24 09:45:45 +0700

    fix (addToCartButton) : add useCart() instead read from local storage

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,18 +1,13 @@
 "use client";
 
 import { Product } from "@/app/types/product";
-// import { useCart } from "@/context/CartContext";
+import { useCart } from "@/context/CartContext";
 
 function AddToCartButton({ product }: { product: Product }) {
-  // const { addToCart } = useCart(); // ambil fungsi dari context
-
-  const handleAddToCart = () => {
-    // addToCart(product); // otomatis update state dan localStorage
-    alert(`${product.title} added to cart!`);
-  };
+  const { addToCart } = useCart();
 
   return (
-    <button onClick={handleAddToCart} className="bg-indigo-600 hover:bg-indigo-700 text-white px-5 py-3 rounded-xl font-semibold transition-all shadow-md">
+    <button onClick={() => addToCart(product)} className="bg-indigo-600 hover:bg-indigo-700 text-white px-5 py-3 rounded-xl font-semibold transition-all shadow-md">
       Add to cart
     </button>
   );
